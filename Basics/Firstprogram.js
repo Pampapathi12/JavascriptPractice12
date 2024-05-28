f42333fff6f0a506b88a67c01e8c251d265ad46c
Pampapathi K <pampapathi@kalajitha.com>
2024-05-28 22:46:38 +0530

    first program

---
+++ Basics/Firstprogram.js
@@ -0,0 +1,5 @@
+// run the javascript programs in vs code in local 
+// node .\test23.js   -> command
+
+// 1. take the input from user input and print using the nodeJS environment
+// 2. using the require and readlin from the javascript
