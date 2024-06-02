5f336a07b8a8936a5526c1d3832957c726a1a278
Pampapathi K <pampapathi@kalajitha.com>
2024-06-02 23:37:10 +0530

    code

---
+++ codingchalleges/practice.js
@@ -0,0 +1,17 @@
+// print the number 1 to 10
+
+for(let i = 0; i <= 10; i++)
+    {
+        console.log(i);
+}
+
+// print the odd number 1 to 100
+
+for( let i=0; i<=100; i++){
+
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
+
+// print the multiplication table with 7
